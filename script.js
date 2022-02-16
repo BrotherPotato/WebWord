145d212baa1cb91782f6418df70dd51ca903d915
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-02-16 23:03:47 +0100

    s

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ var boardState = [];
 var evaluations = []; 
 var rowIndex = 0;
 
+
+
+$(document).ready(function(){
+    $("#inputfile").change(function(){
+        var file = 'textFiles\lemmas_60k.txt';
+        
+        var fReader = new FileReader();
+
+        fReader.addEventListener("load", function() {
+            // this will then display a text file
+            var text = fReader.result;
+          }, false);
+            
+        fr.readAsText(file);
+    });
+
+    if(text != null){
+        alert('asdasd');
+    }
+
+
+});
