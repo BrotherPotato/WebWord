3068017bfca23345348b4d563ebb4836a7970aeb
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-02-16 22:41:10 +0100

    s

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // add code
+
+
+class Word {
+    constructor(wordText, freq) {
+      this.wordText = wordText;
+      this.freq = freq;
+    }
+  }
+
+
+var listOfWords = [];
+var solution;
+var size = 5;
+var boardState = [];
+var evaluations = []; 
+var rowIndex = 0;
+
