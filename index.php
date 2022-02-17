93e800931fd52655d564f4c404b459cc36251c0f
Philip Robertsson <91731687+PhilipRobertsson@users.noreply.github.com>
2022-02-17 15:10:13 +0100

    it's a start

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php include 'includeFiles/StartHead.txt';?>
+    <title> Liurdle</title>
+<?php include 'includeFiles/EndHead.txt';?>
+
+<div id="tileBox" class="center">
+	<div id="row1">
+		<div id = "t1" class="tile">
+		
+		</div>
+		<div id = "t2" class="tile">
+		
+		</div>
+		<div id = "t3" class="tile">
+		
+		</div>
+		<div id = "t4" class="tile">
+		
+		</div>
+		<div id = "t5" class="tile">
+		
+		</div>
+	</div>
+	<div id="row2">
+		<div id = "t1" class="tile">
+		
+		</div>
+		<div id = "t2" class="tile">
+		
+		</div>
+		<div id = "t3" class="tile">
+		
+		</div>
+		<div id = "t4" class="tile">
+		
+		</div>
+		<div id = "t5" class="tile">
+		
+		</div>
+	</div>
+	<div id="row3">
+		<div id = "t1" class="tile">
+		
+		</div>
+		<div id = "t2" class="tile">
+		
+		</div>
+		<div id = "t3" class="tile">
+		
+		</div>
+		<div id = "t4" class="tile">
+		
+		</div>
+		<div id = "t5" class="tile">
+		
+		</div>
+	</div>
+	<div id="row4">
+		<div id = "t1" class="tile">
+		
+		</div>
+		<div id = "t2" class="tile">
+		
+		</div>
+		<div id = "t3" class="tile">
+		
+		</div>
+		<div id = "t4" class="tile">
+		
+		</div>
+		<div id = "t5" class="tile">
+		
+		</div>
+	</div>
+	<div id="row5">
+		<div id = "t1" class="tile">
+		
+		</div>
+		<div id = "t2" class="tile">
+		
+		</div>
+		<div id = "t3" class="tile">
+		
+		</div>
+		<div id = "t4" class="tile">
+		
+		</div>
+		<div id = "t5" class="tile">
+		
+		</div>
+	</div>
+	<div id="row6">
+		<div id = "t1" class="tile">
+		
+		</div>
+		<div id = "t2" class="tile">
+		
+		</div>
+		<div id = "t3" class="tile">
+		
+		</div>
+		<div id = "t4" class="tile">
+		
+		</div>
+		<div id = "t5" class="tile">
+		
+		</div>
+	</div>
+</div>
+
+<div id="keyboard" class="center">
+	<h1>Temp tangentbord</h1>
+</div>
